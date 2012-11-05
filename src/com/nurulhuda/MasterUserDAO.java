10043fbf88e8ec75aa77623f5fd6526ed60f7022
Nurul Huda Mustaqim <kevinmustaqim@gmail.com>
2012-11-05 11:35:53 +0700

    First File

---
+++ src/com/nurulhuda/MasterUserDAO.java
@@ -0,0 +1,32 @@
+/*
+ *  DILARANG MENGUBAH ATAU MENGHAPUS HAK CIPTA INI.
+ * 
+ *  Hak Cipta 2012 Nurul Huda Mustaqim.
+ * 
+ *  Semua isi dalam file ini adalah hak milik dari Nurul Huda Mustaqim.
+ *  Anda tak diperkenankan untuk menggunakan file atau mengubah file ini
+ *  kecuali Anda tidak menghapus atau merubah hak cipta ini.
+ * 
+ *  Pemilik tidak bertanggung jawab jika Anda menyalah gunakan file ini
+ *  dan Pemilik TIDAK MENJAMIN apapun atas file ini.
+ * 
+ *  Anda bisa menghubungi pemilik hak cipta ini di :
+ *  email : kevinmustaqim@gmail.com
+ *  fb  : http://www.facebook.com/nurulhudamustaqim
+ *  ym : office_404
+ */
+package com.nurulhuda;
+
+/**
+ *
+ * @author Office
+ */
+public class MasterUserDAO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
